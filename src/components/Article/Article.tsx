93a4947714cf559e0616e4df2871fe36873be7b0
wildnhl <minnesotanhlwild@gmail.com>
2024-08-26 19:11:59 +0300

    feat(article): create single article component

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,3 +1,42 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useGetSingleArticleQuery } from '../../lib/api/articleApi';
 export function Article() {
-  return <div>Article</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { data, isSuccess } = useGetSingleArticleQuery(id);
+  console.log(data);
+
+  if (isSuccess) {
+    return (
+      <>
+        <button
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          To the articles
+        </button>
+        <div>
+          <img
+            style={{ maxWidth: '700px', margin: '0 auto' }}
+            src={data.image_url}
+            alt="image"
+          />
+          <h2 style={{ textAlign: 'center' }}>{data.title}</h2>
+          <p>{data.summary}</p>
+          <p>
+            Source:{' '}
+            <a
+              style={{ color: 'white', textDecoration: 'none' }}
+              href={data.url}
+            >
+              {data.url}
+            </a>
+          </p>
+        </div>
+      </>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
